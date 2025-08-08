b46794a564b09dd5a00fe80fc431c8e6011fcdb5
tyler-bartschi <tylerabartschi@gmail.com>
2025-08-08 16:53:27 -0600

    Working on updating compression

--- compression_project/compress/main.cpp
+++ compression_project/compress/main.cpp
@@ -12,10 +12,13 @@ using std::unordered_set;
 using std::unordered_map;
 #include <vector>
 using std::vector;
+#include <filesystem>
 #include "Token.h"
 #include "Scanner.h"
 #include "Parser.h"
 #include "Compressor.h"
+
+namespace fs = std::filesystem;
 
 const unordered_map<TokenType, string> token_lookup = {
     // map of TokenTypes to strings, intended for the str() method of Token
@@ -32,45 +35,72 @@ const unordered_set<char> single_values = {
     '\'', '"', ',', '<', '.', '>', '/', '?', ' ', '\n', '\t', '\r'
 };
 
+int validate_infile(const fs::path &input) {
+    if (!fs::exists(input)) {
+        cerr << "Invalid argument: " << input.filename().string() << " is an invalid path." << endl;
+        return 2;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         cerr << "Invalid number of arguments." << endl;
-        cerr << "Usage: ./<executable> <infile> <outfile>" << endl;
+        cerr << "Usage: ./<executable> <input file or folder> <output file or folder>" << endl;
         return 1;
     }
 
-    ifstream in;
-    in.open(argv[1]);
-    if (!in.is_open()) {
-        cerr << "Unable to open file: " << argv[1] << endl;
-        return 1;
+    const fs::path input = argv[1];
+    const fs::path output = argv[2];
+
+    if (validate_infile(input) == 2) {
+        return 2;
     }
 
-    stringstream ss;
-    ss << in.rdbuf();
-    string input = ss.str();
-    in.close();
-
-    Scanner s(input);
-    vector<Token> tokens = s.tokenize();
-    // cout << s.str() << endl;
-
-    Parser p(tokens);
-    // cout << p.print_frequencies() << endl;
-    Heap &tuples = p.generate_compressibles();
-    Compressor c;
-    const unordered_map<string, string> compressed = c.compress(tuples);
-
-    const string result = c.compress_tokens(tokens, compressed);
-
-    ofstream out;
-    out.open(argv[2]);
-    if (!out.is_open()) {
-        cerr << "Unable to open file: " << argv[2] << endl;
-        return 1;
+    if (fs::is_regular_file(input) && fs::is_regular_file(output)) {
+        // both files, can proceed with file compression
+    } else if (fs::is_directory(input) && fs::is_directory(output)) {
+        // both folders, can proceed with folder compression
+    } else if (fs::is_regular_file(input) && fs::is_directory(output)) {
+        // file input, place into a folder for output
+    } else {
+        cerr << "Invalid arguments. Must be two files, two folders, or an input file and an output folder." << endl;
+        cerr << "Cannot be an input folder and an output file." << endl;
+        return 2;
     }
-    out << result;
-    out.close();
 
-    return 0;
+    // ifstream in;
+    // in.open(argv[1]);
+    // if (!in.is_open()) {
+    //     cerr << "Unable to open file: " << argv[1] << endl;
+    //     return 1;
+    // }
+    //
+    // stringstream ss;
+    // ss << in.rdbuf();
+    // string input = ss.str();
+    // in.close();
+    //
+    // Scanner s(input);
+    // vector<Token> tokens = s.tokenize();
+    // // cout << s.str() << endl;
+    //
+    // Parser p(tokens);
+    // // cout << p.print_frequencies() << endl;
+    // Heap &tuples = p.generate_compressibles();
+    // Compressor c;
+    // const unordered_map<string, string> compressed = c.compress(tuples);
+    //
+    // const string result = c.compress_tokens(tokens, compressed);
+    //
+    // ofstream out;
+    // out.open(argv[2]);
+    // if (!out.is_open()) {
+    //     cerr << "Unable to open file: " << argv[2] << endl;
+    //     return 1;
+    // }
+    // out << result;
+    // out.close();
+    //
+    // return 0;
 }
