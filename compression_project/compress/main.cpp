ddca8b30242088fc890e6f55b31528b99a91244a
tyler-bartschi <tylerabartschi@gmail.com>
2025-08-09 10:51:45 -0600

    Update file compression

--- compression_project/compress/main.cpp
+++ compression_project/compress/main.cpp
@@ -35,12 +35,65 @@ const unordered_set<char> single_values = {
     '\'', '"', ',', '<', '.', '>', '/', '?', ' ', '\n', '\t', '\r'
 };
 
+bool is_text_file(const fs::path &file_path, const size_t &check_size = 4096) {
+    ifstream in(file_path, std::ios::binary);
+
+    if (!in.is_open()) {
+        return false;
+    }
+
+    size_t count = 0;
+    char ch;
+
+    while (count < check_size && in.get(ch)) {
+        if (ch == '\0') {
+            return false;
+        }
+        count++;
+    }
+    return true;
+}
+
 int validate_infile(const fs::path &input) {
     if (!fs::exists(input)) {
         cerr << "Invalid argument: " << input.filename().string() << " is an invalid path." << endl;
         return 2;
     }
     return 0;
+}
+
+void check_if_open(ifstream &file, const string &filename) {
+    if (!file.is_open()) {
+        stringstream msg;
+        msg << "Error: " << filename << " could not be opened." << endl;
+        throw std::invalid_argument(msg.str());
+    }
+}
+
+void check_if_open(ofstream &file, const string &filename) {
+    if (!file.is_open()) {
+        stringstream msg;
+        msg << "Error: " << filename << " could not be opened." << endl;
+        throw std::invalid_argument(msg.str());
+    }
+}
+
+void do_file_compression(const string &input, const fs::path &output) {
+    Scanner s(input);
+    const vector<Token> tokens = s.tokenize();
+
+    Parser p(tokens);
+    Heap &tuples = p.generate_compressibles();
+
+    Compressor c;
+    const unordered_map<string, string> compressed = c.compress(tuples);
+    const string result = c.compress_tokens(tokens, compressed);
+
+    ofstream out;
+    out.open(output);
+    check_if_open(out, output.filename().string());
+    out << result;
+    out.close();
 }
 
 int main(int argc, char *argv[]) {
@@ -59,6 +112,20 @@ int main(int argc, char *argv[]) {
 
     if (fs::is_regular_file(input) && fs::is_regular_file(output)) {
         // both files, can proceed with file compression
+        if (!is_text_file(input)) {
+            cerr << "Invalid argument: " << input.filename().string() << " is not a text file." << endl;
+            return 2;
+        }
+        ifstream in;
+        in.open(input);
+        check_if_open(in, input.filename().string());
+
+        stringstream ss;
+        ss << in.rdbuf();
+        string file_contents = ss.str();
+        in.close();
+
+        do_file_compression(file_contents, output);
     } else if (fs::is_directory(input) && fs::is_directory(output)) {
         // both folders, can proceed with folder compression
     } else if (fs::is_regular_file(input) && fs::is_directory(output)) {
@@ -68,6 +135,8 @@ int main(int argc, char *argv[]) {
         cerr << "Cannot be an input folder and an output file." << endl;
         return 2;
     }
+
+    return 0;
 
     // ifstream in;
     // in.open(argv[1]);
