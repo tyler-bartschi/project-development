3ecdd0a5202c37e7cbda0694c52b9e50c2a4bcb8
tyler-bartschi <tylerabartschi@gmail.com>
2025-08-10 16:44:33 -0600

    Update Decompressor.h

--- compression_project/decompress/Decompressor.h
+++ compression_project/decompress/Decompressor.h
@@ -26,21 +26,4 @@ public:
         }
         return out.str();
     }
-
-    // static string decompress(const vector<Token> &tokens, const unordered_map<string, string> &decoding_table) {
-    //     stringstream out;
-    //     for (const auto& token : tokens) {
-    //         if (token.get_type() == SINGLE) {
-    //             out << token.get_value();
-    //         } else if (token.get_type() == DIGIT || token.get_type() == STRING) {
-    //             const string& key = token.get_value();
-    //             if (decoding_table.find(key) == decoding_table.end()) {
-    //                 std::cerr << "Missing key in decoding_table: '" << key << "'\n";
-    //                 throw std::runtime_error("Key not found in decoding_table");
-    //             }
-    //             out << decoding_table.at(key);
-    //         }
-    //     }
-    //     return out.str();
-    // }
 };
